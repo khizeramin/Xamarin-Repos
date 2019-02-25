07bb0827a7b24e5b4a063fb4f7d9a48605592e04
Khizer Amin <engrkhizeramin@gmail.com>
2019-02-25 16:00:27 +0100

    Creating a repo

---
+++ ThingSlider/ThingSlider/SliderBehavior.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ThingSlider
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SliderBehavior : ContentPage
+	{
+		public SliderBehavior ()
+		{
+			InitializeComponent ();
+            //Content = new Label
+            //{
+            //    Text = "Hello"
+            //};
+            localImage.Source = ImageSource.FromFile("background.jpg");
+            //label.FontSize = 18;
+            //label.Text = "Hello";
+            //slider.Value = 0;   
+ 		}
+         
+        
+        //void slideMode()
+        //{
+             
+        //    //Slider slider = new Slider()
+        //    //{
+        //    //    Maximum = 360
+        //    //};
+            
+        //}
+        
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            //slideMode();
+            if (e.NewValue > 0)
+            {
+                //colorChange.BackgroundColor = "Black";
+                localImage.Source = ImageSource.FromFile("background_2.jpg"); 
+            }
+             
+             
+        }
+    }
+}
